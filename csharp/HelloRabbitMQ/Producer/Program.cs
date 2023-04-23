f66606c9fb5d56694c87879b6acaf06a5b7d34a9
lukaofirst <lucasmacedo123@gmail.com>
2023-04-22 21:26:29 -0300

    HelloRabbitMQ - first commit with a Producer and Consumer

---
+++ csharp/HelloRabbitMQ/Producer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+const string rabbitMQAddress = "localhost";
+
+var factory = new ConnectionFactory() { HostName = rabbitMQAddress };
+//factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+using var conn = factory.CreateConnection();
+using var channel = conn.CreateModel();
+
+channel.QueueDeclare("HelloRabbitQueue", false, false, false, null);
+
+var message = "I'm just getting started with RabbitMQ";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty, "HelloRabbitQueue", null, body);
+
+Console.WriteLine("Message sent to queue...");
---
+++ csharp/HelloRabbitMQ/Receiver/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using static System.Console;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var conn = factory.CreateConnection();
+using var channel = conn.CreateModel();
+
+channel.QueueDeclare("HelloRabbitQueue", false, false, false);
+channel.BasicQos(0, 1, false);
+
+WriteLine("Getting messages from queue");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+	var body = ea.Body.ToArray();
+	var message = Encoding.UTF8.GetString(body);
+
+	WriteLine($"Result: {message}");
+};
+
+channel.BasicConsume("HelloRabbitQueue", true, consumer);
+
+ReadLine();
