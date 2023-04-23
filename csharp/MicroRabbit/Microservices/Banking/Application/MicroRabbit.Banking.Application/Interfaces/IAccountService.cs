0c0107f01b31dd87aa0a6d92aad6294b3f97f8a9
lukaofirst <lucasmacedo123@gmail.com>
2023-04-23 15:41:50 -0300

    MicroRabbit - implementing the Banking's application layer

---
+++ csharp/MicroRabbit/Microservices/Banking/Application/MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,9 @@
+using MicroRabbit.Banking.Domain.Models;
+
+namespace MicroRabbit.Banking.Application.Interfaces
+{
+	public interface IAccountService
+	{
+		IEnumerable<Account> GetAccounts();
+	}
+}
