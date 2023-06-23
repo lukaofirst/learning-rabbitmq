cab9cffa772aea0fcbbe424f187033b051461591
lukaofirst <lucasmacedo123@gmail.com>
2023-06-22 21:52:28 -0300

    MultiExchanges - Direct

---
+++ csharp/MultiExchanges/01 - Direct/Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) => 
+{
+	var body = ea.Body.ToArray();
+	var receivedMessage = Encoding.UTF8.GetString(body);
+
+	Console.WriteLine($"Received Message: {receivedMessage}");
+
+	channel.BasicAck(ea.DeliveryTag, false);
+};
+
+channel.BasicConsume("example-queue", false, consumer);
+
+Console.ReadKey();
---
+++ csharp/MultiExchanges/01 - Direct/Producer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("DirectExchange", ExchangeType.Direct);
+channel.QueueDeclare("example-queue", false, false, false, null);
+channel.QueueBind("example-queue", "DirectExchange", "routingKey");
+
+Console.WriteLine("How many message do you want to create?");
+var times = int.Parse(Console.ReadLine()!);
+
+
+for (int i = 0; i < times; i++)
+{
+	var message = Encoding.UTF8.GetBytes($"Message #{i}");
+
+	channel.BasicPublish("DirectExchange", "routingKey", null, message);
+}
