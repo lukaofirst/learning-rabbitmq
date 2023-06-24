8fb4274ac253949197a9dd0536e6ab466f8503a9
lukaofirst <lucasmacedo123@gmail.com>
2023-06-24 13:59:09 -0300

    MultiExchanges - Dead Letter

---
+++ csharp/MultiExchanges/06 - Dead Letter/Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("dead-letter-queue", false, false, false, null);
+channel.QueueBind("dead-letter-queue", "DeadLetterExchange", "dead-letter-routing-key");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+	var body = ea.Body.ToArray();
+	var message = Encoding.UTF8.GetString(body);
+
+	Console.WriteLine($"Received message: {message}");
+
+	channel.BasicAck(ea.DeliveryTag, false);
+};
+
+channel.BasicConsume("dead-letter-queue", false, consumer);
+
+Console.ReadKey();
---
+++ csharp/MultiExchanges/06 - Dead Letter/Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("DeadLetterExchange", ExchangeType.Direct);
+
+channel.QueueDeclare("dead-letter-queue", false, false, false, null);
+channel.QueueBind("dead-letter-queue", "DeadLetterExchange", "dead-letter-routing-key");
+
+var arguments = new Dictionary<string, object>()
+{
+	{ "x-dead-letter-exchange", "DeadLetterExchange" },
+	{ "x-dead-letter-routing-key", "dead-letter-routing-key" }
+};
+
+channel.QueueDeclare("main-queue", false, false, false, arguments);
+
+Console.WriteLine("How many message do you want to create?");
+var times = int.Parse(Console.ReadLine()!);
+var rnd = new Random();
+
+for (int i = 0; i < times; i++)
+{
+	var message = Encoding.UTF8.GetBytes($"Message #{i}");
+	var properties = channel.CreateBasicProperties();
+	properties.Expiration = "5000";
+
+	/* Use an empty string as the first argument, the default exchange type is been used here (Direct) */
+
+	/* 
+	   The second argument specifies the routing key for the message, 
+	   but in this case, the routing key is the name of the queue,
+	   because of the default exchange type
+	*/
+	channel.BasicPublish(string.Empty, "main-queue", properties, message);
+}
