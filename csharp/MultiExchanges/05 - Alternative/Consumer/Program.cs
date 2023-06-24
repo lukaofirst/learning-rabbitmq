26e303bc89fe2b1b287da7391b5793f63d269604
lukaofirst <lucasmacedo123@gmail.com>
2023-06-24 13:06:40 -0300

    MultiExchanges - Alternative

---
+++ csharp/MultiExchanges/05 - Alternative/Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var arguments = new Dictionary<string, object>() 
+{
+	{ "alternate-exchange", "AlternateExchange" }
+};
+
+channel.ExchangeDeclare("DExchange", ExchangeType.Direct, arguments: arguments);
+channel.ExchangeDeclare("AlternateExchange", ExchangeType.Fanout);
+
+channel.QueueDeclare("example-queue", false, false, false, null);
+channel.QueueBind("example-queue", "AlternateExchange", string.Empty);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+	var body = ea.Body.ToArray();
+	var message = Encoding.UTF8.GetString(body);
+
+	Console.WriteLine($"Received message: {message}");
+
+	channel.BasicAck(ea.DeliveryTag, false);
+};
+
+channel.BasicConsume("example-queue", false, consumer);
+
+Console.ReadKey();
---
+++ csharp/MultiExchanges/05 - Alternative/Producer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var arguments = new Dictionary<string, object>() 
+{
+	{ "alternate-exchange", "AlternateExchange" }
+};
+
+channel.ExchangeDeclare("DExchange", ExchangeType.Direct, arguments: arguments);
+channel.ExchangeDeclare("AlternateExchange", ExchangeType.Fanout);
+
+Console.WriteLine("How many message do you want to create?");
+var times = int.Parse(Console.ReadLine()!);
+var rnd = new Random();
+
+for (int i = 0; i < times; i++)
+{
+	var message = Encoding.UTF8.GetBytes($"Message #{i}");
+
+	channel.BasicPublish("DExchange", "non-existing-routingKey", null, message);
+}
